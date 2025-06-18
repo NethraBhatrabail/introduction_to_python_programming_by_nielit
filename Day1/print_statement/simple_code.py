95a20860eb161d110e5b09ea2588a74780226973
NethraBhatrabail <160316723+NethraBhatrabail@users.noreply.github.com>
2025-06-18 21:27:40 +0530

    Delete Day1/print_statement directory

--- Day1/print_statement/simple_code.py
+++
@@ -1,7 +0,0 @@
-# Making ur computer speak
-# go to tool and then manage packages to install any packages
-# search for packages pyttsx3- text to speech TTS libraray
-import pyttsx3
-engine=pyttsx3.init()
-engine.say("I am learning python")
-engine.runAndWait()
